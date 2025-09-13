08fe58c949ec6160ff9e87aed941266f8f5ed5bd
Sahas <sahasmcg2995@gmail.com>
2025-09-13 21:49:00 +0530

    Update vars

--- app/config/variable.py
+++ app/config/variable.py
@@ -10,17 +10,17 @@ def parse_list(env: str) -> list:
 
 HOST_ENV = os.getenv('HOST_ENV', 'local')
 
-GRAFANA_BASE_URL = os.getenv('GRAFANA_BASE_URL')
-SERVICES = parse_list(os.getenv('SERVICES', ''))
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'info')
+GRAFANA_BASE_URL = os.getenv('GRAFANA_BASE_URL', 'http://159.138.171.113:3100')
+SERVICES = parse_list(os.getenv('SERVICES', 'jam-bp-jam-lk-api,troika-bp-smile-th-api'))
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'ERROR')
 QUERY = parse_list(os.getenv('QUERY', ''))
 EXCLUSIONS = parse_list(os.getenv('EXCLUSIONS', ''))
 FREQUENCY = os.getenv('FREQUENCY', '30')
 
 EMAIL_SERVER = os.getenv('EMAIL_SERVER', 'smtp.gmail.com')
 EMAIL_PORT = os.getenv('EMAIL_PORT', 465)
-EMAIL_USER_NAME = os.getenv('EMAIL_USER_NAME')
-EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_USER_NAME = os.getenv('EMAIL_USER_NAME', 'shamikakosgolle@gmail.com')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD', 'iwey ppky zugh fyvc')
 EMAIL_RECIPIENTS = parse_list(os.getenv('EMAIL_RECIPIENT'))
 
 LOGO_URL = os.getenv('LOGO_URL')
